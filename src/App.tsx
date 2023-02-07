cfd6983b9443b9b1a9b63b397fb43555e3ec4ca9
JamieOVO <104012860+JamieOVO@users.noreply.github.com>
2023-02-07 09:43:44 +0000

    hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/header/Header";
-import Ebss from "./screens/ebss/Ebss";
-import Home from "./screens/home/Home";
-import Smhc from "./screens/smhc/Smhc";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Ebss from './screens/ebss/Ebss';
+import Home from './screens/home/Home';
+import Smhc from './screens/smhc/Smhc';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/smart-meter-troubleshooting-tool" element={<Smhc />} />
           <Route path="/winter-support-tool" element={<Ebss />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
